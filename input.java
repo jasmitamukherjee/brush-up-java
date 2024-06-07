c4bc7cf883872e913ae680dec65ea095936aa195
jasmitamukherjee <jasmitamukherjee4@gmail.com>
2024-06-07 21:05:41 +0530

    Update input.java

--- input.java
+++ input.java
@@ -3,9 +3,13 @@ import java.util.*;
 public class input {
     public static void main(String args[]){
         Scanner sc= new Scanner(System.in);
-        System.out.println("Enter your name : ");
-        String name = sc.nextLine();
-        System.out.println("You entered : "+ name);
+        // System.out.println("Enter your name : ");
+        // String name = sc.nextLine();
+        // System.out.println("You entered : "+ name);
+        System.out.println("Enter a and b : ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("Sum of "+a+" and "+b+" is "+(a+b));
         sc.close();
     }
 }
