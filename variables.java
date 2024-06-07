9bec8a8cc72861f00392a0ceec3997bc289b327e
jasmitamukherjee <jasmitamukherjee4@gmail.com>
2024-06-07 20:38:29 +0530

    print formats

---
+++ variables.java
@@ -0,0 +1,13 @@
+public class variables {
+    public static void main(String args[]){
+        System.out.print("hey\n");
+        System.out.print("there\n");
+        System.out.print("cutie"+"\n");
+        System.out.print("pie\n");
+
+
+
+
+    }
+    
+}
