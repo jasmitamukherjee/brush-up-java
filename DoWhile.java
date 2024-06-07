e44777f9606637cfb12d0aca8df099f6d2869c94
jasmitamukherjee <jasmitamukherjee4@gmail.com>
2024-06-07 21:32:40 +0530

    Create DoWhile.java

---
+++ DoWhile.java
@@ -0,0 +1,11 @@
+public class DoWhile {
+    public static void main(String args[]){
+        int i=0;
+        do{
+            //gets executed atleast once no matter what
+            System.out.println(i);
+            i++;
+        }while(i<0);
+    }
+    
+}
