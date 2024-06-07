8b98341f95b2bc5087914c65cb080405eb2bb1da
jasmitamukherjee <jasmitamukherjee4@gmail.com>
2024-06-07 21:00:50 +0530

    variables

--- variables.java
+++ variables.java
@@ -1,9 +1,22 @@
 public class variables {
     public static void main(String args[]){
-        System.out.print("hey\n");
-        System.out.print("there\n");
-        System.out.print("cutie"+"\n");
-        System.out.print("pie\n");
+        // System.out.print("hey\n");
+        // System.out.print("there\n");
+        // System.out.print("cutie"+"\n");
+        // System.out.print("pie\n");
+
+        //variable start
+        String name = "Jasmita";
+        int x=5;
+        int y=10;
+        double z = 25.36;
+        System.out.println(name + "\n"+ x+ "\n"+y + "\n"+z); 
+        double res = x+z ;
+        int diff = x-y;
+        System.out.println(res);
+System.out.println(x*y / x-y);
+System.out.println((x*y) / (x-y));
+
 
 
 
