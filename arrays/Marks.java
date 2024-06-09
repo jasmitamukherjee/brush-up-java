ee90e96e2743ee239adc60269b777c57ccee19c5
jasmitamukherjee <jasmitamukherjee4@gmail.com>
2024-06-09 11:06:44 +0530

    Create Marks.java

---
+++ arrays/Marks.java
@@ -0,0 +1,15 @@
+package arrays;
+
+public class Marks {
+
+    public static void main(String[] args) {
+        int[] marks =new int[3];
+        marks[0]=78;
+        marks[1]=100;
+        marks[2]=150;
+        for(int i=0;i<3;i++)
+        System.out.println(marks[i]);
+
+    }
+    
+}
