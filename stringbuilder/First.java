b0bf3bd459f56320a05ebda46164414aaa31a69b
jasmitamukherjee <jasmitamukherjee4@gmail.com>
2024-07-01 11:02:53 +0530

    string builder

--- stringbuilder/First.java
+++ stringbuilder/First.java
@@ -10,6 +10,15 @@ public class First {
         System.out.println(sb.charAt(0));
       sb.setCharAt(0, 'L');
       System.out.println(sb);
+      sb.insert(1, 'K');
+      System.out.println(sb);
+      sb.delete(1, 2);
+      System.out.println(sb);
+      sb.deleteCharAt(4);
+      System.out.println(sb);
+      sb.append('x');
+      System.out.println(sb);
+      System.out.println(sb.length());
     }
 
     
